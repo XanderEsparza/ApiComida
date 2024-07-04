2583228e2554a6b663dfdf769a4288e8b670339c
Odalys Garcia <odalytasgarcia22@gmail.com>
2024-07-03 19:20:38 -0600

    Ingresar actualizacion

--- controllers/comidaController.js
+++ controllers/comidaController.js
@@ -1,1 +1,31 @@
 const Comida = require('../models/comidaModel');
+
+
+// comidaController.js
+const Comida = require('../models/comidaModel');
+
+const actualizarComida = async (req, res) => {
+    const { id } = req.params;
+    const { nombre, precio, descripcion, existencia } = req.body;
+
+    try {
+        const comidaActualizada = await Comida.findByIdAndUpdate(
+            id,
+            { nombre, precio, descripcion, existencia },
+            { new: true, runValidators: true }
+        );
+
+        if (!comidaActualizada) {
+            return res.status(404).json({ message: 'Comida no encontrada' });
+        }
+
+        res.json(comidaActualizada);
+    } catch (error) {
+        res.status(500).json({ message: 'Error actualizando la comida', error });
+    }
+};
+
+module.exports = {
+    actualizarComida,
+   
+};
