3290e178fea490c0c500fbf0daefcb88905b2250
Antonia Ortiz <antoniaaoc79@gmail.com>
2024-07-08 22:39:48 -0600

    Creacion del controlador eliminar

--- controllers/comidaController.js
+++ controllers/comidaController.js
@@ -1,1 +1,18 @@
 const Comida = require('../models/comidaModel');
+
+const eliminarComidaPorNombre = async (req, res) => {
+  try {
+    const nombreComida = req.params.nombre;
+    const comidaEliminada = await Comida.findOneAndDelete({ nombre: nombreComida });
+
+    if (!comidaEliminada) {
+      return res.status(404).json({ mensaje: 'Comida no encontrada' });
+    }
+
+    res.status(200).json({ mensaje: 'Comida eliminada exitosamente', comida: comidaEliminada });
+  } catch (error) {
+    res.status(500).json({ mensaje: 'Error eliminando la comida', error });
+  }
+};
+  
+module.exports={ eliminarComidaPorNombre}
